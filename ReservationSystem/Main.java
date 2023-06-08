a9a26bbe69ea379d289c23d40bb429c233adec03
KATTA NAGA NITHIN <88046362+nithinkatta@users.noreply.github.com>
2023-06-08 21:18:14 +0530

    Add files via upload

---
+++ ReservationSystem/Main.java
@@ -0,0 +1,40 @@
+package ReservationSystem;
+import java.io.IOException;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static Connection jdbc()
+    {
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","phpmyadmin","PassWord123");
+            
+            Statement stmt = con.createStatement();
+//            ResultSet rs = stmt.executeQuery("select * from Passengers");
+//            while(rs.next())
+//            {
+//                System.out.println(rs.getString(1)+"  "+rs.getString(2));  
+//                
+//            }
+//            con.close();
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return con;
+    }
+    public static void main(String[] args) throws IOException
+    {
+        LoginPage lp = new LoginPage();
+        lp.setVisible(true);
+        lp.pack();
+        lp.setLocationRelativeTo(null);
+//          JOptionPane.showMessageDialog(null, "connected");
+
+        String s = lp.username;
+        
+    }
+}
